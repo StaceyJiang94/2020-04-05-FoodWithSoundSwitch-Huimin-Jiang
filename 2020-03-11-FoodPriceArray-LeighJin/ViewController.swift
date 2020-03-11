17c5925504c6ed9af39a6c7c35e19536a4d0d3f9
Leigh Jin <jinlei@sfsu.edu>
2020-03-11 13:31:57 -0700

    Initial Commit

---
+++ 2020-03-11-FoodPriceArray-LeighJin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-11-FoodPriceArray-LeighJin
+//
+//  Created by Leigh Jin on 3/11/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
