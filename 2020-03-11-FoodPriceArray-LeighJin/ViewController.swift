3e0e8b0b5e50f73e3f332deddee049e6e8cb6291
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-03-31 21:20:51 -0700

    HW6 _ Food Array With Sound App

--- 2020-03-11-FoodPriceArray-LeighJin/ViewController.swift
+++ 2020-03-11-FoodPriceArray-LeighJin/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var foodImageView: UIImageView!
+    
+    
+    @IBOutlet weak var playSoundSwitch: UISwitch!
+    
+    
+    
+    let foodImageArray = ["chicken","hamburger","noodle","pasta","pizza","salad","steak"]
+    
+    let foodSoundArray = ["applause02", "cheering01", "cheering02", "gong", "guitar", "magic", "movie"]
+    
+    
+    var foodIndex = 0
+    
+    //define audio player
+    var audioPlayer: AVAudioPlayer!
+    
+    //create function that allows us to play audio
+    func playSound(soundName: String) {
+        //involve if-let syntax to unwrap optionals
+        if let sound = NSDataAsset(name: soundName) {
+            //deal with do-try-catch
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription) Could not initalize AVAudioPlayer object")
+            }
+            
+        } else {
+            print("ERROR: Could not be data from sound file")
+        }
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        foodImageView.image = UIImage(named: foodImageArray[0])
+        
+        playSound(soundName: foodSoundArray[foodIndex])
     }
 
+    
+    @IBAction func playSoundSwitched(_ sender: UISwitch) {
+//        if !sender.isOn && audioPlayer != nil {
+//            audioPlayer.stop()
+//        }
+        
+        if let playingAudioPlayer = audioPlayer {
+            if !playSoundSwitch.isOn {
+                playingAudioPlayer.stop()
+            }
+        }
+        
+    }
+    
+    @IBAction func nextDishButtonClicked(_ sender: UIButton) {
+        foodIndex += 1
+        if foodIndex == foodImageArray.count {
+            foodIndex = 0
+        }
+        foodImageView.image = UIImage(named: foodImageArray[foodIndex])
+        
+        if playSoundSwitch.isOn {
+            playSound(soundName: foodSoundArray[foodIndex])
+        }
+        
+    }
+    
 
 }
 
